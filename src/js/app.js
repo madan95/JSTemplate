91f66012eddd29d4c15f23ff7252f29462dac126
Madan Limbu <madan_limbu@hotmail.co.uk>
2019-04-03 23:32:23 +0100

    Template for react project

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,2 @@
 import '../scss/style.scss';
-console.log('test');
+import './component/heading/index.js';
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         use: {
           loader:'babel-loader',
           options: {
-            presets: ['@babel/preset-env',] //Babel to convert ES6 to ES5
+            presets: ['@babel/preset-env','@babel/react'] //Babel to convert ES6 to ES5
           },
         },
       },
