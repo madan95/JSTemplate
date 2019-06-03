56611e94120629ee254b256946443832bab65633
madan limbu <madanl@deeson.com>
2019-06-03 22:23:55 +0100

    Prettier module installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ["@babel/polyfill", path.join(__dirname, 'src', 'index.js')], //Entry point of module and add pollyfill before
-  output: { //Where to emit the bundle/static
-    path: path.join(__dirname, 'build'),
-    filename: 'index.bundle.js'
+  entry: ["@babel/polyfill", path.join(__dirname, "src", "index.js")], //Entry point of module and add pollyfill before
+  output: {
+    //Where to emit the bundle/static
+    path: path.join(__dirname, "build"),
+    filename: "index.bundle.js"
   },
-  devtool: 'inline-source-map', //maps to orginal file for debuging
-  mode: process.env.NODE_ENV || 'production', //prod mode if not manually set (webpack optimization in prod)
+  devtool: "inline-source-map", //maps to orginal file for debuging
+  mode: process.env.NODE_ENV || "production", //prod mode if not manually set (webpack optimization in prod)
   resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'] //Helps resolving to relative paths when importing anything form src
+    modules: [path.resolve(__dirname, "src"), "node_modules"] //Helps resolving to relative paths when importing anything form src
   },
   devServer: {
-    contentBase: path.join(__dirname, 'src') //Helps webpack-dev-server on what files to serve in the browser
+    contentBase: path.join(__dirname, "src") //Helps webpack-dev-server on what files to serve in the browser
   },
   /* Loaders, allows webpack to process other types of file and convert them (scss/css/png)
    *
@@ -29,15 +30,16 @@ module.exports = {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: {
-          loader:'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env',] //Babel to convert ES6 to ES5
-          },
-        },
+            presets: ["@babel/preset-env"] //Babel to convert ES6 to ES5
+          }
+        }
       },
       {
         test: /\.(css|scss)$/,
-        use: [ // last to first loader
+        use: [
+          // last to first loader
           "style-loader", //inject css to DOM
           "css-loader", //Interprets imoprt style.css directive to css
           "sass-loader" //compiles sass into css (Node sass)
@@ -45,7 +47,7 @@ module.exports = {
       },
       {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        loaders: ['file-loader'] //Helps resolving (import/require) a file into a url
+        loaders: ["file-loader"] //Helps resolving (import/require) a file into a url
       }
     ]
   },
@@ -54,6 +56,8 @@ module.exports = {
    *
    */
   plugins: [
-    new HtmlWebpackPlugin({template: path.join(__dirname, 'src', 'index.html')})  //Helps injecting JS to html
-  ],
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html")
+    }) //Helps injecting JS to html
+  ]
 };
